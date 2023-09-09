1819cfecb85bcb08641dc09cd48a6d8033d0ae80
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-09-09 14:30:11 +0700

    first commit

---
+++ bin/dart_object_oriented_programming.dart
@@ -0,0 +1,5 @@
+import 'package:dart_object_oriented_programming/dart_object_oriented_programming.dart' as dart_object_oriented_programming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_object_oriented_programming.calculate()}!');
+}
---
+++ test/dart_object_oriented_programming_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_object_oriented_programming/dart_object_oriented_programming.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
